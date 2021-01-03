9ec4e43fba680957adfe2375ae51388bdcd94138
Walter Perdan <info@kalwaltart.it>
2021-01-03 22:37:19 +0100

    display infos for everi images

--- example/js/main.js
+++ example/js/main.js
@@ -3,33 +3,37 @@ var numIset;
 window.addEventListener('dispImageSet-loaded', function() {
   var ariset = new ARiset(893, 1117);
   // We need to load the first Image to get numIset with the listener
-  ariset.loadNFTMarker('data/pinball', 0);  
+  ariset.loadNFTMarker('data/pinball', 0);
     document.addEventListener('nftMarker', function (ev) {
       numIset = ev.detail.numIset;
       // Now we can load the other images
-      for (var i = 1; i < numIset; i++) {
-      ariset.loadNFTMarker('data/pinball', i);
-      }
+      var i = 1;
+          while(i<numIset) { 
+              ariset.loadNFTMarker('data/pinball', i);
+              i++;
+            if (i===numIset){
+              break;
+            }
+          }
       var imageSetWidth = ev.detail.widthNFT;
       var imageSetHeight = ev.detail.heightNFT;
       var dpi = ev.detail.dpi;
-  
       var divNumIset = document.getElementById("numIset");
       var contentNI = document.createTextNode(numIset);
       divNumIset.appendChild(contentNI);
-  
-      var divW = document.getElementById("widthNFT");
-      var contentW = document.createTextNode(imageSetWidth);
-      divW.appendChild(contentW);
-  
-      var divH = document.getElementById("heightNFT");
-      var contentH = document.createTextNode(imageSetHeight);
-      divH.appendChild(contentH);
-  
-      var divDpi = document.getElementById("dpiNFT");
-      var contentDpi = document.createTextNode(dpi);
-      divDpi.appendChild(contentDpi);
-    ariset.display();
+      ariset.display();
+      infos(imageSetWidth, imageSetHeight, dpi);
   })
 })
+
+function infos(imageSetWidth, imageSetHeight, dpi){
+  var parent = document.createElement('div');
+  document.body.appendChild(parent);
+  var contentW = document.createTextNode('Width: ' + imageSetWidth + ' ');
+  parent.appendChild(contentW);
+  var contentH = document.createTextNode('Height: ' + imageSetHeight+ ' ');
+  parent.appendChild(contentH);
+  var contentDpi = document.createTextNode('Dpi: ' + dpi);
+  parent.appendChild(contentDpi);
+}
 
