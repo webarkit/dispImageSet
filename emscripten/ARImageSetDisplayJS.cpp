f453393c035f3766b465fc17eaed0cdaf517b056
kalwalt <info@kalwaltart.it>
2022-05-08 00:26:13 +0200

    create_emscripten_canvas not create all canvases

--- emscripten/ARImageSetDisplayJS.cpp
+++ emscripten/ARImageSetDisplayJS.cpp
@@ -46,133 +46,26 @@ std::unordered_map<int, arIset> arIsets;
 static int ARISET_NOT_FOUND = -1;
 static int gARIsetID = 0;
 
-GLuint program;
-
-#define PIX_C(x, y) ((x)/256.0f + (y)/256.0f)
-#define CLAMP(c) ((c) < 0.f ? 0.f : ((c) > 1.f ? 1.f : (c)))
-#define PIX(x, y) CLAMP(PIX_C(x, y))
-
 extern "C" {
-using emscripten::val;
-void draw()
-{
-  int w, h;
-  emscripten_get_canvas_element_size("#canvas", &w, &h);
-  printf("canvas width: %d\n", w);
-  printf("canvas height: %d\n", h);
-  float xs = (float)h / w;
-  float ys = 1.0f;
-  float mat[] = { xs, 0, 0, 0, 0, ys, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
-  glUniformMatrix4fv(glGetUniformLocation(program, "mat"), 1, 0, mat);
-  glClearColor(0,0,1,1);
-  glClear(GL_COLOR_BUFFER_BIT);
-  glDrawArrays(GL_TRIANGLES, 0, 6);
 
-  unsigned char* imageData = (unsigned char*)malloc(256*256*4*sizeof(unsigned char));
-  glReadPixels(0, 0, 256, 256, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
-  for(int y = 0; y < 256; ++y)
-    for(int x = 0; x < 256; ++x)
-    {
-      unsigned char red = imageData[(y*256+x)*4];
-      float expectedRed = PIX(x, y);
-      unsigned char eRed = (unsigned char)(expectedRed * 255.0f);
-    }
-  emscripten_cancel_main_loop();
-}
-
-//void emscripten_canvas(int width, int height, const char* name, float* texData) {
-void emscripten_canvas(int width, int height, const char* name, unsigned char* texData) {
-  emscripten_set_canvas_element_size(name, width, height);
-  EmscriptenWebGLContextAttributes attr;
-  emscripten_webgl_init_context_attributes(&attr);
-  attr.alpha = attr.depth = attr.stencil = attr.antialias =
-      attr.preserveDrawingBuffer = attr.failIfMajorPerformanceCaveat = 0;
-  attr.enableExtensionsByDefault = 1;
-  attr.premultipliedAlpha = 0;
-  attr.majorVersion = 1;
-  attr.minorVersion = 0;
-  EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx =
-      emscripten_webgl_create_context(name, &attr);
-  emscripten_webgl_make_context_current(ctx);
-  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-  const char *vss = "attribute vec4 vPosition; uniform mat4 mat; varying vec2 "
-                    "texCoord; void main() { gl_Position = vPosition; texCoord "
-                    "= (vPosition.xy + vec2(1.0)) * vec2(0.5); }";
-  glShaderSource(vs, 1, &vss, 0);
-  glCompileShader(vs);
-  GLint isCompiled = 0;
-  glGetShaderiv(vs, GL_COMPILE_STATUS, &isCompiled);
-  if (!isCompiled) {
-    GLint maxLength = 0;
-    glGetShaderiv(vs, GL_INFO_LOG_LENGTH, &maxLength);
-    char *buf = new char[maxLength];
-    glGetShaderInfoLog(vs, maxLength, &maxLength, buf);
-    printf("%s\n", buf);
-  }
-
-  GLuint ps = glCreateShader(GL_FRAGMENT_SHADER);
-  const char *pss = "precision lowp float; varying vec2 texCoord; uniform vec3 "
-                    "colors[3]; uniform sampler2D tex; void main() { "
-                    "gl_FragColor = texture2D(tex, texCoord); }";
-  glShaderSource(ps, 1, &pss, 0);
-  glCompileShader(ps);
-  glGetShaderiv(ps, GL_COMPILE_STATUS, &isCompiled);
-  if (!isCompiled) {
-    GLint maxLength = 0;
-    glGetShaderiv(ps, GL_INFO_LOG_LENGTH, &maxLength);
-    char *buf = new char[maxLength];
-    glGetShaderInfoLog(ps, maxLength, &maxLength, buf);
-    printf("%s\n", buf);
-  }
-
-  program = glCreateProgram();
-  glAttachShader(program, vs);
-  glAttachShader(program, ps);
-  glBindAttribLocation(program, 0, "vPosition");
-  glLinkProgram(program);
-  glGetProgramiv(program, GL_LINK_STATUS, &isCompiled);
-  if (!isCompiled) {
-    GLint maxLength = 0;
-    glGetShaderiv(program, GL_INFO_LOG_LENGTH, &maxLength);
-    char *buf = new char[maxLength];
-    glGetProgramInfoLog(program, maxLength, &maxLength, buf);
-    printf("%s\n", buf);
-  }
-
-  glUseProgram(program);
-
-  GLuint vbo;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-  float verts[] = {-1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1};
-  glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 2, GL_FLOAT, 0, sizeof(float) * 2, 0);
-  glEnableVertexAttribArray(0);
-  GLuint tex;
-  glGenTextures(1, &tex);
-  glBindTexture(GL_TEXTURE_2D, tex);
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  /*glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, width, height, 0, GL_LUMINANCE,
-               GL_FLOAT, texData);*/
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, width, height, 0, GL_LUMINANCE,
-               GL_UNSIGNED_BYTE, texData);
-  emscripten_set_main_loop(draw, 0, 0);
-}
-
-void emscripten_canvas2(int width, int height, std::string name, unsigned char* texData)
+void create_emscripten_canvas(int id, int width, int height, unsigned char* texData)
 {
   EM_ASM_({
-    var canvas = document.getElementById("canvas");
+    var canvas = document.createElement("canvas");
+    var idx = $0;
+    var canvasId = "canvas" + idx;
+    canvas.id = canvasId;
+    console.log("canvas id: ", canvasId);
     var ctx = canvas.getContext("2d");
-    var width = $0;
-    var height = $1;
+    var width = $1;
+    var height = $2;
+    console.log("canvas width: ", width);
+    console.log("canvas height: ", height);
     ctx.canvas.width = width;
     ctx.canvas.height = height;
     var Buffer = new Uint8ClampedArray(
       Module.HEAPU8.buffer,
-      $2,
+      $3,
       width*height*4
     );
     var id = new ImageData(
@@ -191,18 +84,11 @@ void emscripten_canvas2(int width, int height, std::string name, unsigned char* 
     };
     ctx.putImageData(id, 0, 0);
   },
+  id,
   width,
   height,
   texData
   );
-}
-
-void emscripten_canvas3()
-{
-  val canvas = val::global("document").call<val>("getElementById", val("canvas"));
-  val ctx = canvas.call<val>("getContext", val("2d"));
-  ctx.set("fillStyle", "green");
-  ctx.call<void>("fillRect", 10, 10, 150, 100);
 }
 
 int loadNFTMarker(arIset *arc, int surfaceSetCount, int numImage,
@@ -257,17 +143,10 @@ nftMarker readNFTMarker(int id, int numImage, std::string datasetPathname) {
   }
 
   arc->surfaceSetCount++;
-  int width = 256;
-  int height = 256;
-  float *floatTexData = (float *)malloc(width * height * sizeof(float));
-  for (int y = 0; y < height; ++y)
-    for (int x = 0; x < width; ++x) {
-      floatTexData[y * width + x] = PIX(x, y);
-    }
-  //emscripten_canvas(256, 256, "#canvas", (unsigned char*)floatTexData);
-  emscripten_canvas2(arc->width_NFT, arc->height_NFT, "canvas", arc->imgBW);
-  //emscripten_canvas3();
-  //emscripten_canvas(256, 256, "#canvas", arc->imgBW);
+  int numIset = arc->numIset;
+  for(int i = 0; i < numIset; i++){
+    create_emscripten_canvas(i, arc->imageSet->scale[i]->xsize, arc->imageSet->scale[i]->ysize, arc->imageSet->scale[i]->imgBW);
+  }
 
   EM_ASM_(
       {
