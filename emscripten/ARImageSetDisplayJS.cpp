44efb9307354a9c9b9f35268cbe32682d7bc868f
kalwalt <github@kalwaltart.it>
2022-05-09 00:27:49 +0200

    simplified code

--- emscripten/ARImageSetDisplayJS.cpp
+++ emscripten/ARImageSetDisplayJS.cpp
@@ -5,40 +5,17 @@
 #include <AR2/imageSet.h>
 #include <AR2/tracking.h>
 #include <AR2/util.h>
-#include <GLES2/gl2.h>
 #include <KPM/kpm.h>
 #include <emscripten.h>
-#include <emscripten/html5.h>
-#include <emscripten/val.h>
 #include <stdio.h>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
-#define PAGES_MAX  10 // Maximum number of pages expected. You can change this down (to save
-                      // memory) or up (to accomodate more pages.)
-
-struct nftMarker {
-  int numIset;
-  int widthNFT;
-  int heightNFT;
-  int dpiNFT;
-  int imgBWsize;
-  int pointer;
-};
-
 struct arIset {
   int id = 0;
-  int width = 0;
-  int height = 0;
-  ARUint8 *imgBW = NULL;
-  int imgBWsize;
   AR2ImageSetT *imageSet = NULL;
   int numIset;
-  int width_NFT;
-  int height_NFT;
-  int dpi_NFT;
-  int surfaceSetCount = 0; // Running NFT marker id
 };
 
 std::unordered_map<int, arIset> arIsets;
@@ -93,10 +70,8 @@ void create_emscripten_canvas(int id, int width, int height, unsigned char* texD
   );
 }
 
-int loadNFTMarker(arIset *arc, int surfaceSetCount, int numImage,
-                  const char *filename) {
+int loadNFTMarker(arIset *arc, const char *filename) {
   ARLOGi("Read ImageSet.\n");
-  ARLOGi("numImage is: %d\n", numImage);
   ar2UtilRemoveExt((char *)filename);
   arc->imageSet = ar2ReadImageSet((char *)filename);
   if (arc->imageSet == NULL) {
@@ -106,65 +81,39 @@ int loadNFTMarker(arIset *arc, int surfaceSetCount, int numImage,
   ARLOGi("  end.\n");
 
   arc->numIset = arc->imageSet->num;
-  arc->width_NFT = arc->imageSet->scale[numImage]->xsize;
-  arc->height_NFT = arc->imageSet->scale[numImage]->ysize;
-  arc->dpi_NFT = arc->imageSet->scale[numImage]->dpi;
-  arc->imgBW = arc->imageSet->scale[numImage]->imgBW;
-
-  ARLOGi("printing pointer imgBW: %d\n", arc->imgBW);
-
-  ARLOGi("NFT number of ImageSet: %i\n", arc->numIset);
-  ARLOGi("NFT marker width: %i\n", arc->width_NFT);
-  ARLOGi("NFT marker height: %i\n", arc->height_NFT);
-  ARLOGi("NFT marker dpi: %i\n", arc->dpi_NFT);
-
-  ARLOGi("imgBW filled\n");
-
-  ARLOGi("  Done.\n");
 
   ARLOGi("Loading of NFT data complete.\n");
   return (TRUE);
 }
 
-nftMarker readNFTMarker(int id, int numImage, std::string datasetPathname) {
-  nftMarker nft;
+bool readNFTMarker(int id, std::string datasetPathname) {
   if (arIsets.find(id) == arIsets.end()) {
-    return nft;
+    return false;
   }
   arIset *arc = &(arIsets[id]);
 
   // Load marker(s).
-  int patt_id = arc->surfaceSetCount;
-  if (!loadNFTMarker(arc, patt_id, numImage, datasetPathname.c_str())) {
+  if (!loadNFTMarker(arc, datasetPathname.c_str())) {
     ARLOGe("ARimageFsetDisplay(): Unable to read NFT marker.\n");
-    return nft;
+    return false;
   } else {
-    ARLOGi("Passing the imgBW pointer: %d\n", (int)arc->imgBW);
+    ARLOGi("Creating all the canvases...\n");
   }
 
-  arc->surfaceSetCount++;
   int numIset = arc->numIset;
   for(int i = 0; i < numIset; i++){
     create_emscripten_canvas(i, arc->imageSet->scale[i]->xsize, arc->imageSet->scale[i]->ysize, arc->imageSet->scale[i]->imgBW);
   }
 
-  nft.numIset = arc->numIset;
-  nft.widthNFT = arc->width_NFT;
-  nft.heightNFT = arc->height_NFT;
-  nft.dpiNFT = arc->dpi_NFT;
+  ARLOGi("  Done.\n");
 
-  return nft;
+  return true;
 }
 
-int setup(int width, int height) {
+int setup() {
   int id = gARIsetID++;
   arIset *arc = &(arIsets[id]);
   arc->id = id;
-  arc->imgBWsize = width * height * 4 * sizeof(ARUint8);
-  arc->imgBW = (ARUint8 *)malloc(arc->imgBWsize);
-
-  ARLOGi("Allocated imgBWsize %d\n", arc->imgBWsize);
-
   return arc->id;
 }
 }
--- emscripten/bindings.cpp
+++ emscripten/bindings.cpp
@@ -7,11 +7,4 @@ EMSCRIPTEN_BINDINGS(constant_bindings) {
   function("setup", &setup);
   function("_readNFTMarker", &readNFTMarker);
 
-  value_object<nftMarker>("nftMarker")
-  .field("numIset", &nftMarker::numIset)
-  .field("width", &nftMarker::widthNFT)
-  .field("height", &nftMarker::heightNFT)
-  .field("dpi", &nftMarker::dpiNFT)
-  .field("imgBWsize", &nftMarker::imgBWsize)
-  .field("pointer", &nftMarker::pointer);
 }
